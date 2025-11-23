056c81b1d3b6c46b36e0a20096536c7758940cd5
ishaandesai <IshaanDesai21@gmail.com>
2025-11-22 22:56:15 -0600

    Ishaan TeleOp Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtlAtl_Decode/TeleOp/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtlAtl_Decode/TeleOp/TeleOpTest.java
@@ -1,4 +1,105 @@
 package org.firstinspires.ftc.teamcode.AtlAtl_Decode.TeleOp;
 
-public class TeleOpTest {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class TeleOpTest extends OpMode {
+    private DcMotorEx leftFront, rightFront, leftBack, rightBack;
+    private DcMotorEx bottomIntake, topIntake;
+    private DcMotorEx transfer;
+    private DcMotorEx shooter;
+
+    @Override
+    public void init() {
+        //Initialize Drivetrain -----------------------------------------------------
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+
+        leftFront.setDirection(DcMotorEx.Direction.FORWARD);
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBack.setDirection(DcMotorEx.Direction.FORWARD);
+        rightBack.setDirection(DcMotorEx.Direction.REVERSE);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Initialize Intakes -----------------------------------------------------
+        bottomIntake = hardwareMap.get(DcMotorEx.class, "bIntake");
+        topIntake = hardwareMap.get(DcMotorEx.class, "tIntake");
+
+        bottomIntake.setDirection(DcMotorEx.Direction.FORWARD);
+        topIntake.setDirection(DcMotorEx.Direction.FORWARD);
+
+        //Initialize Transfer -----------------------------------------------------
+        transfer = hardwareMap.get(DcMotorEx.class, "transfer");
+        transfer.setDirection(DcMotorEx.Direction.FORWARD);
+
+        //Initialize Shooter -----------------------------------------------------
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        shooter.setDirection(DcMotorEx.Direction.FORWARD);
+
+    }
+
+    @Override
+    public void loop() {
+        Drive();
+        Intake();
+        Transfer();
+        Shooter();
+    }
+
+    public void Drive() {
+        double strafe   = -gamepad1.left_stick_x;
+        double vertical = -gamepad1.left_stick_y;
+        double heading  =  gamepad1.right_stick_x;
+
+        double leftFrontPower  = vertical + strafe + heading;
+        double rightFrontPower = vertical - strafe - heading;
+        double leftBackPower   = vertical - strafe + heading;
+        double rightBackPower  = vertical + strafe - heading;
+
+        double max = Math.max(1.0, Math.max(
+                Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower)),
+                Math.max(Math.abs(leftBackPower), Math.abs(rightBackPower))
+        ));
+
+        leftFront.setPower(leftFrontPower / max);
+        rightFront.setPower(rightFrontPower / max);
+        leftBack.setPower(leftBackPower / max);
+        rightBack.setPower(rightBackPower / max);
+    }
+
+    public void Intake() {
+        //if a, power in 0.9
+        //if y, power out 0.9
+        //if none clicked, 0
+
+        //optionally you can have it default to intake always on by putting that in the else case
+        if (gamepad1.a) {
+            topIntake.setPower(0.9);
+            bottomIntake.setPower(0.9);
+        } else if (gamepad1.y) {
+            topIntake.setPower(-0.9);
+            bottomIntake.setPower(-0.9);
+        } else {
+            topIntake.setPower(0);
+            bottomIntake.setPower(0);
+        }
+    }
+
+    public void Transfer() {
+        //TODO - Make code
+    }
+
+    public void Shooter() {
+        //TODO - Make code
+    }
+
+
+
 }
