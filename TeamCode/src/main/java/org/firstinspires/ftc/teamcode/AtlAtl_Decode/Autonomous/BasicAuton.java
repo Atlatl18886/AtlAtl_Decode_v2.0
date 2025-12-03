2af9d19435b870496254a1fedfc97a8fb0f7378e
jayanp <jayanp0202@gmail.com>
2025-12-02 20:10:55 -0600

    test auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtlAtl_Decode/Autonomous/BasicAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtlAtl_Decode/Autonomous/BasicAuton.java
@@ -1,174 +1,204 @@
 package org.firstinspires.ftc.teamcode.AtlAtl_Decode.Autonomous;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.AtlAtl_Decode.TeleOp.TeleOpConfig;
 
+@Autonomous(name="BasicAuton", group="V2")
 public class BasicAuton extends LinearOpMode {
-    //Declare Motors
-    //private double tick = 45.2763689698;
-    private DcMotorEx leftFront;
-    private DcMotorEx rightFront;
-    private DcMotorEx leftBack;
-    private DcMotorEx rightBack;
-    private DcMotorEx intake;
-    private DcMotorEx transfer;
-    private DcMotorEx shooter;
+    private DcMotorEx leftFront, rightFront, leftBack, rightBack;
+    private DcMotorEx intake, transfer, shooter;
+    private IMU imu;
+
+    private static final double TICKS_PER_REV = 537.6; //gobilda 5203s 13.7:1
+    private static final double WHEEL_DIAMETER_IN = 104.0 / 25.4;
+    private static final double WHEEL_CIRC = WHEEL_DIAMETER_IN * Math.PI;
+
+    //calibration factor to correct for mecanum wheel slippage.
+    private static final double CALIBRATION_FACTOR = 1.1;
+    private static final double TICKS_PER_INCH = (TICKS_PER_REV / WHEEL_CIRC) * CALIBRATION_FACTOR;
 
     @Override
     public void runOpMode() {
-
-        //---------------- INITIALIZATION ----------------\\
-        //Drive Motor Config
         leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
         leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
         rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        resetEncoders();
 
-        //Resets all encoder values to 0
-        leftFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rightFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        leftBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rightBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        //Sets motor target positions to 0
-        leftFront.setTargetPosition(0);
-        rightFront.setTargetPosition(0);
-        leftBack.setTargetPosition(0);
-        rightBack.setTargetPosition(0);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Set RunMode to Run to Position
-        leftFront.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        leftBack.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        rightBack.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.resetYaw();
 
-        //When power of 0 is applied, it will brake, rather than glide with inertia
-        leftFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        rightFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        leftBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        rightBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-
-
-        //TODO: Set motor directions here, when a positive power is applied, it should go forward
-        leftFront.setDirection(DcMotorEx.Direction.FORWARD);
-        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
-        leftBack.setDirection(DcMotorEx.Direction.FORWARD);
-        rightBack.setDirection(DcMotorEx.Direction.REVERSE);
-
-        // init everything else
         intake = hardwareMap.get(DcMotorEx.class, "intake");
-        intake.setDirection(DcMotorEx.Direction.FORWARD);
-        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        intake.setPower(0);
-
+        intake.setDirection(DcMotor.Direction.REVERSE);
         transfer = hardwareMap.get(DcMotorEx.class, "transfer");
-        transfer.setDirection(DcMotorEx.Direction.REVERSE);
-        transfer.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        transfer.setPower(0);
-
+        transfer.setDirection(DcMotorSimple.Direction.REVERSE);
         shooter = hardwareMap.get(DcMotorEx.class, "shooter");
-        shooter.setDirection(DcMotorEx.Direction.REVERSE);
+
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        transfer.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        intake.setPower(0);
+        transfer.setPower(0);
         shooter.setPower(0);
 
-        //--------------------- RUN ---------------------\\
-        //waits for the Driver Hub to receive "start" input
+        telemetry.addData("Status", "Ready");
+        telemetry.update();
 
         waitForStart();
 
-        //TODO: Pathing, use the functions below to move the robot
+        //TODO: PATHING
         transfer.setPower(-0.8);
-        intake(1, 4);
-        vertical(10, 0.5, 2);
+        intake.setPower(1.0);
+        drive(12, 5.0, 3.0);
+        intake.setPower(0);
+        strafe(12, 3.0, 3.0);
+        turnToHeading(90, 3.0);
+        turnToHeading(0, 3.0);
     }
 
-    //Positive value will go forward, Negative value will go backward
-    public void vertical(int ticks, double speed, long seconds) {
-        reset();
+//helper methods
+    private int inchesToTicks(double inches) {
+        return (int)(inches * TICKS_PER_INCH);
+    }
+    private double revsPerSecToVelocity(double revsPerSec) {
+        return revsPerSec * TICKS_PER_REV;
+    }
+    public void drive(double inches, double revsPerSec, double timeoutSec) {
+        resetEncoders();
+        int ticks = inchesToTicks(inches);
+        double velocity = revsPerSecToVelocity(revsPerSec);
 
         leftFront.setTargetPosition(ticks);
         leftBack.setTargetPosition(ticks);
         rightFront.setTargetPosition(ticks);
         rightBack.setTargetPosition(ticks);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        setAll(velocity);
 
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
-
-        sleep(seconds * 1000);
+        ElapsedTime timer = new ElapsedTime();
+        while (opModeIsActive() && isBusy() && timer.seconds() < timeoutSec) {
+            telemetry.addData("Path", "Driving " + inches + " inches");
+            telemetry.addData("Target", ticks);
+            telemetry.addData("Current", leftFront.getCurrentPosition());
+            telemetry.update();
+        }
+        stopAll();
     }
 
-    //Positive value will strafe right, Negative value will strafe left
-    public void strafe(int ticks, double speed, long seconds) {
-        reset();
-
-        leftFront.setTargetPosition(ticks);
-        leftBack.setTargetPosition(-ticks);
+    public void strafe(double inches, double revsPerSec, double timeoutSec) {
+        resetEncoders();
+        int ticks = inchesToTicks(inches);
+        double velocity = revsPerSecToVelocity(revsPerSec);
+        leftFront.setTargetPosition(-ticks);
+        leftBack.setTargetPosition(ticks);
         rightFront.setTargetPosition(-ticks);
         rightBack.setTargetPosition(ticks);
 
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        setAll(velocity);
 
-        sleep(seconds * 1000);
-    }
-
-    //Positive value will turn right, Negative value will turn left
-    public void turn(int ticks, double speed, long seconds) {
-        reset();
-
-        leftFront.setTargetPosition(-ticks);
-        leftBack.setTargetPosition(-ticks);
-        rightFront.setTargetPosition(ticks);
-        rightBack.setTargetPosition(ticks);
-
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
-
-        sleep(seconds * 1000);
-    }
-
-    public void reset() {
-        //Resets all encoder values to 0
-        leftFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rightFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        leftBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rightBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-
-        //Sets target position to 0
-        leftFront.setTargetPosition(0);
-        rightFront.setTargetPosition(0);
-        leftBack.setTargetPosition(0);
-        rightBack.setTargetPosition(0);
-
-        //Sets motor mode to Run to Position to make sure no motor has a target position set (0)
-        leftFront.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        leftBack.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        rightBack.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    }
-
-    private ElapsedTime intakeTimer = new ElapsedTime();
-    private boolean intakeActive = false;
-
-    public void intake(double power, double seconds) {
-        if (!intakeActive) {
-            intake.setPower(power);
-            intakeTimer.reset();
-            intakeActive = true;
+        ElapsedTime timer = new ElapsedTime();
+        while (opModeIsActive() && isBusy() && timer.seconds() < timeoutSec) {
+            telemetry.addData("Path", "Strafing " + inches + " inches");
+            telemetry.update();
         }
-
-        if (intakeActive && intakeTimer.seconds() >= seconds) {
-            intake.setPower(0);
-            intakeActive = false;
-        }
+        stopAll();
     }
 
+    public void turnToHeading(double targetHeading, double timeoutSec) {
+        ElapsedTime timer = new ElapsedTime();
+        ElapsedTime pdTimer = new ElapsedTime();
+        double prevError = 0;
+        double kP = TeleOpConfig.imu_kP; // * TeleOpConfig.imu_turn_factor;
+        double kD = TeleOpConfig.imu_kD;
+
+        while (opModeIsActive() && timer.seconds() < timeoutSec) {
+            double currentHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+            double error = targetHeading - currentHeading;
+
+            //limit error
+            while (error > 180) error -= 360;
+            while (error <= -180) error += 360;
+
+            if (Math.abs(error) < 2.0) {
+                break;
+            }
+
+            double dt = pdTimer.seconds();
+            pdTimer.reset();
+            if (dt < 1e-3) dt = 1e-3;
+
+            double derivative = (error - prevError) / dt;
+            prevError = error;
+
+            double turnPower = (error * kP) + (derivative * kD);
+
+            if (turnPower > 0.6) turnPower = 0.6;
+            if (turnPower < -0.6) turnPower = -0.6;
+            if (Math.abs(turnPower) < 0.05) turnPower = Math.copySign(0.05, turnPower);
+
+            leftFront.setPower(turnPower);
+            leftBack.setPower(turnPower);
+            rightFront.setPower(-turnPower);
+            rightBack.setPower(-turnPower);
+            telemetry.addData("Target", targetHeading);
+            telemetry.addData("Current", currentHeading);
+            telemetry.addData("Error", error);
+            telemetry.update();
+        }
+        stopAll();
+    }
+
+    // utility functions
+    private void resetEncoders() {
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    private void setAll(double velocity) {
+        leftFront.setVelocity(velocity);
+        leftBack.setVelocity(velocity);
+        rightFront.setVelocity(velocity);
+        rightBack.setVelocity(velocity);
+    }
+    private void stopAll() {
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+    }
+    private boolean isBusy() {
+        return leftFront.isBusy() || rightFront.isBusy() || leftBack.isBusy() || rightBack.isBusy();
+    }
 }
