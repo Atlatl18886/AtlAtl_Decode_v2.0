2d735b0b49192766ea91f14d97a46d318bb3b6aa
jayanp <jayanp0202@gmail.com>
2025-12-02 20:11:19 -0600

    shooter test for tps weird stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtlAtl_Decode/TeleOp/ShooterTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.AtlAtl_Decode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+@TeleOp
+public class ShooterTest extends OpMode {
+    private DcMotorEx shooter;
+    @Override
+    public void init() {
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        shooter.setDirection(DcMotorEx.Direction.REVERSE);
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter.setPower(0);
+    }
+    @Override
+    public void loop() {
+        Shooter();
+    }
+    public void Shooter() {
+        if (gamepad1.y) {
+            shooter.setVelocity(ShooterConfig.CLOSE_TPS);
+        }
+    }
+}
+
