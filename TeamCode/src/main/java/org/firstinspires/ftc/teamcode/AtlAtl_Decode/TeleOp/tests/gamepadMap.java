3bb58d3f73d5b9a33a2d05ddff86fffefc6a7024
KingJayan <jayanp0202@gmail.com>
2026-02-04 21:55:45 -0600

    abdefg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtlAtl_Decode/TeleOp/tests/gamepadMap.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.AtlAtl_Decode.TeleOp.tests;
+
+public class gamepadMap {
+}
+//we use Sony/PS DUALSHOCK 4,
+//jank ones are logitech f310
+//xbox that dont work are Microsoft Xbox 360 for Windows
